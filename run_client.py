6258c083ff82a9268dacb1c00c49400976785947
Malhar Bhoite <malhar.bhoite@gmail.com>
2024-11-05 00:07:56 -0500

    Move run scripts to scripts directory

--- run_client.py
+++
@@ -1,5 +0,0 @@
-from mbta_home_app.client_gui import GUI
-    
-
-if __name__ == "__main__":
-    GUI("KENDALL/MIT")
--- run_server.py
+++
@@ -1,5 +0,0 @@
-from mbta_home_app.server import *
-
-if __name__=="__main__":
-    server = MBTAHomeAppServer()
-    server.run_server()
